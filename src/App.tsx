cec7deca31dbc3f72f7de039898cd712c16cc1a8
Arssin <mateusz.binieda@onet.pl>
2022-05-11 00:09:37 +0200

    feat: add hello react handler

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleHelloAlert = () => {
+    alert('hello react')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button onClick={handleHelloAlert}> Show alert!</button>
         <a
           className="App-link"
           href="https://reactjs.org"
