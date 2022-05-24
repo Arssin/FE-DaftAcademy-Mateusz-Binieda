e275454d7929f07dbf0144db0d8db6d31d050e83
Arssin <mateusz.binieda@onet.pl>
2022-05-25 00:27:57 +0200

    feature(navbar/homepage): code refactor,add routing, add components

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import './App.css';
 import {ShowAlert} from './components/ShowAlert/ShowAlert'
 import {InputForm} from './components/InputForm'
 import {Footer} from './components/Footer'
+import {Navbar} from './components/Navbar'
+import {Homepage} from './components/Homepage'
 import {Header} from './components/Header'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <div className="Container">
-        <ShowAlert />
-        <InputForm  defaultValue={'Hello There'}/>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/showAlert' element ={<ShowAlert />} />
+          <Route path='/form' element = {<InputForm  defaultValue={'Hello There'}/>} />
+        </Routes>
       </div>
       <Footer />
     </div>
