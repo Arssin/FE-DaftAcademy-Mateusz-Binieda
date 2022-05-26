2729850f244c7a7fad204f6f8d9ab0fab7dd30c2
Arssin <mateusz.binieda@onet.pl>
2022-05-26 12:48:35 +0200

    feature(contact): styling

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,13 +1,25 @@
 import styles from './Contact.module.scss';
 import pikachu from '../../images/pikachu.gif'
-import {FaFacebookSquare}from "react-icons/fa";
+import {FaFacebookSquare, FaInstagram, FaGithub, FaLinkedin}from "react-icons/fa";
 
 export  function Contact() {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.contactContainer}>You can contact with me via: 
-        <FaFacebookSquare />
-        <FaFacebookSquare />
+      <div className={styles.contactContainer}>
+        You can contact with me via: 
+      <br/>
+      <a href="https://www.facebook.com/mateusz.binieda.14/" rel="noreferrer" target="_blank">
+        <FaFacebookSquare className={styles.icons}/>
+      </a>
+      <a href="https://www.instagram.com/songo_bjj/" rel="noreferrer" target="_blank">
+        <FaInstagram className={styles.icons}/>
+      </a>
+      <a href="https://github.com/Arssin" rel="noreferrer" target="_blank"> 
+        <FaGithub  className={styles.icons}/>
+      </a>
+      <a href="https://www.linkedin.com/in/mateusz-bini%C4%99da-818141209/" rel="noreferrer" target="_blank">
+        <FaLinkedin  className={styles.icons}/>
+      </a>
       </div>
       <img className={styles.gif} src={pikachu} alt='pikachu gif' />
     </div>
