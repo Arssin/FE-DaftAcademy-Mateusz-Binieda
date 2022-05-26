15fd2be93af44fe77e1a730ad97315a4371866f0
Arssin <mateusz.binieda@onet.pl>
2022-05-26 03:32:04 +0200

    feature(contact): add icons, add text, add gif

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,15 @@
+import styles from './Contact.module.scss';
+import pikachu from '../../images/pikachu.gif'
+import {FaFacebookSquare}from "react-icons/fa";
+
 export  function Contact() {
   return (
-    <div>Contact</div>
+    <div className={styles.mainContainer}>
+      <div className={styles.contactContainer}>You can contact with me via: 
+        <FaFacebookSquare />
+        <FaFacebookSquare />
+      </div>
+      <img className={styles.gif} src={pikachu} alt='pikachu gif' />
+    </div>
   )
 }
